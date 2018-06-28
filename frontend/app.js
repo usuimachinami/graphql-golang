e6d4f3e4edf1aaa40829af565e83d3e5acb8f307
usuimachinami <usuimachinami@gmail.com>
2018-06-28 22:48:31 +0900

    Fixed expired token

--- frontend/app.js
+++ frontend/app.js
@@ -4,34 +4,42 @@ const vm = new Vue({
         results: []
     },
     mounted() {
-        // var instance = axios.create({
-        //     baseURL: 'http://localhost:5000',
-        //     timeout: 1000,
-        //     headers: {'content-type': 'text/plain;charset=UTF-8', 'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiZXhwIjoxNTE0MTE0MjcwLCJuYW1lIjoidGVzdHVzZXIifQ.F2uHXNfz8JgdOr_Ey9gkMCApw6mfDtOxHkJZCZJqfro'}
-        // });
-        var configGraphQL = {
-            url: 'http://localhost:5000/query',
-            method: 'post',
-            headers: { 'Content-Type': 'application/graphql;charset=UTF-8', 'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiZXhwIjoxNTE0MTE0MjcwLCJuYW1lIjoidGVzdHVzZXIifQ.F2uHXNfz8JgdOr_Ey9gkMCApw6mfDtOxHkJZCZJqfro' },
-            data: 'query { titles (order: "") { name, created_at, stories { name } } }'
-        };
-        axios(configGraphQL)
-            .then(response => {
+        getToken().then(token => {
+            var configGraphQL = {
+                url: 'http://127.0.0.1:5000/query',
+                method: 'post',
+                headers: { 'Content-Type': 'application/graphql;charset=UTF-8', 'authorization': 'Bearer ' + token },
+                data: 'query { titles (order: "") { name, created_at, stories { name } } }'
+            };
+            axios(configGraphQL).then(response => {
                 var titles = response.data.data.titles;
                 for (var i in titles) {
                     titles[i].created_at = dateFormat(new Date(titles[i].created_at))
                 }
                 this.results = titles
             });
+        });
     }
 });
+
+async function getToken() {
+    var configGraphQL = {
+        url: 'http://127.0.0.1:5000/login',
+        method: 'post',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
+        data: 'username=testuser&password=testpassword'
+    };
+    const res = await axios(configGraphQL);
+
+    return res.data.token;
+}
 
 function dateFormat(date) {
     var y = date.getFullYear();
     var m = date.getMonth() + 1;
     var d = date.getDate();
     var w = date.getDay();
-    var wNames = ['Sun.', 'Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sta.'];
+    var wNames = ['Sun.', 'Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.'];
 
     m = ('0' + m).slice(-2);
     d = ('0' + d).slice(-2);
